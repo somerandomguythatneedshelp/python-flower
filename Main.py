d5d37a63cfbd53a264fe774efd50a70e849929f6
somerandomguythatneedshelp <107365137+somerandomguythatneedshelp@users.noreply.github.com>
2024-05-10 21:35:23 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+import turtle as tr
+
+tr.setup(800,800)
+
+s = tr.Screen()
+
+s.bgcolor("#262626")
+tr.pencolor("#540d6e")
+tr.speed(10000000000100000000000000010000000000000001000000000000000100000000000000010000000000000001000000000000000100000000000000010000000000000001000000000000000100000000000000000000)
+tr.tracer(100)
+tr.pensize(1)
+col = ("#FF7F3F", "#FBDF07", "#89CFFD", "#F94892")
+
+for i in range(3):
+    for n in range(400):
+        tr.color(col[n%4])
+        tr.circle(190-n/2,90)
+        tr.left(90)
+        tr.circle(190-n/2,90)
+        tr.color(col[n%4])
+    tr.left(30)
+s.exitonclick()
